baf84ef88a2a432de5bcf1cc47da8a5902eef1a9
Juan Montoya <jcmontoya@gmail.com>
2018-10-02 16:30:06 -0400

    Working with reference types, value types, immutability, and arrays

---
+++ GradeBook/Grades.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Grades.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Grades.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d161e745-4ad6-4113-8636-343182a03cae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Sleepy/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Sleepy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How many hours of sleep did you get last night?");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello, " + name);
+            if(hoursOfSleep > 8)
+            {
+                System.Console.WriteLine("You are well rested.");
+            }
+            else
+            {
+                System.Console.WriteLine("You need more sleep");
+            }
+        }
+    }
+}
